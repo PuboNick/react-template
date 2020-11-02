9ec52fc0378a57b97f1aef180b503101e339eed5
蒋金明 <C3005993>
2020-11-02 09:48:35 +0800

    feat(fonts): 引入roboto字體

--- src/plugins/dva.ts
+++ src/plugins/dva.ts
@@ -1,10 +1,17 @@
 import { getDvaApp } from 'umi';
 
-interface StateRes<T> {
-  error?: string;
-  state?: T;
-  success: boolean;
+interface StateSuccess<T> {
+  success: true;
+  error: null;
+  state: T;
 }
+interface StateFalse<T> {
+  success: false;
+  error: string;
+  state: null;
+}
+
+type StateRes<T> = StateSuccess<T> | StateFalse<T>;
 
 /**
  * 獲取dva store 數據
@@ -12,7 +19,7 @@ interface StateRes<T> {
  */
 export const getState = <T>(namespace: string): StateRes<T> => {
   let store = getDvaApp();
-  if (!store) return { error: '獲取dva失敗!', success: false };
+  if (!store) return { error: '獲取dva失敗!', success: false, state: null };
   let state: T = store._store.getState()[namespace];
-  return { state, success: true };
+  return { state, success: true, error: null };
 };
