5c47bd08b89c2dae4736add57848f127dc6ddef7
蒋金明 <C3005993>
2020-12-17 09:28:31 +0800

    feat(home):刪除多餘代碼

--- src/pages/main/home/index.tsx
+++ src/pages/main/home/index.tsx
@@ -1,8 +1,6 @@
 import React, { FC } from 'react';
-import { IndexModelState, useSelector } from 'umi';
 
 const IndexPage: FC = () => {
-  const index: IndexModelState = useSelector((state: any) => state.index);
   return <div style={{ width: '100%', height: '100vh' }}>Home page</div>;
 };
 
