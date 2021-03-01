ecef9374808ccf22c3bbaae356c7580a0a92e992
蒋金明 <C3005993>
2021-03-01 14:28:12 +0800

    feat(bootstrap):增加取消監聽功能

--- src/plugins/bootstrap/index.ts
+++ src/plugins/bootstrap/index.ts
@@ -1,5 +1,10 @@
+import { random } from '../utils';
+
 /**
  * app 生命週期管理器
+ * @member on 監聽器
+ * @member cancel 取消監聽
+ * @member handle 分發事件
  * events:
  * init 項目初始化時
  * login 登陸成功後，參數 user {User} 用戶信息
@@ -9,23 +14,34 @@
  * getToken 獲取到 access_token 時, 參數 access_token {字符串}
  */
 interface BootstrapType {
-  state: any;
-  on: (event: string, func: any) => any;
+  on: (event: string, func: any) => string;
+  cancel: (id: string) => void;
   handle: (event: string, ...args: any) => any;
 }
 
+// 方法緩存
+const state: any = {};
+// id 緩存
+const ids: any = {};
+
 const bootstrap: BootstrapType = {
-  state: {},
   on(event: string, func: any) {
-    if (!bootstrap.state[event]) bootstrap.state[event] = [];
-    if (typeof func !== 'function') return;
-    bootstrap.state[event].push(func);
+    if (!state[event]) state[event] = [];
+    if (typeof func !== 'function') throw '第二個參數必須為方法!';
+    const index = state[event].push(func) - 1;
+    const key = random() + random();
+    ids[key] = { event, index };
+    return key;
+  },
+  cancel(id: string) {
+    const { event, index } = ids[id] || {};
+    if (!event) return;
+    if (!Array.isArray(state[event])) return;
+    state[event].splice(index, 1);
   },
   async handle(event: string, ...args: any) {
-    if (Array.isArray(bootstrap.state[event])) {
-      const funList = bootstrap.state[event];
-      for (let i = 0; i < funList.length; i++) {
-        const func = funList[i];
+    if (Array.isArray(state[event])) {
+      for (const func of state[event]) {
         if (typeof func === 'function') await func(...args);
       }
     }
